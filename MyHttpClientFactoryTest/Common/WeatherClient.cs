378c27b35129ed22f5a18022317e3d94b6a14651
Hakim Ghods <ghods.hakim@hotmail.com>
2020-05-17 07:53:10 +0430

    Add project files.

---
+++ MyHttpClientFactoryTest/Common/WeatherClient.cs
@@ -0,0 +1,39 @@
+﻿using MyHttpClientFactoryTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MyHttpClientFactoryTest.Common
+{
+    public class WeatherClient
+    {
+        public HttpClient Client { get; }
+        public WeatherClient(HttpClient client)
+        {
+            client.BaseAddress = new Uri("http://localhost:5500/");
+            client.DefaultRequestHeaders.Add("Auth", "my-bearer-token");
+
+            client.DefaultRequestHeaders.Add("User-Agent", "Codehaks Sample App");
+
+
+            Client = client;
+        }
+
+        public async Task<IEnumerable<WeatherInfo>> GetWeatherInfo()
+        {
+            var response = await Client.GetAsync("/weatherforecast");
+
+            response.EnsureSuccessStatusCode();
+            var data = await response.Content.ReadAsStringAsync();
+
+            var results = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<WeatherInfo>>(data);
+
+            return results;
+        }
+
+
+    }
+}
---
+++ MyHttpClientFactoryTest/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MyHttpClientFactoryTest.Common;
+using MyHttpClientFactoryTest.Models;
+
+namespace MyHttpClientFactoryTest.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly WeatherClient _weatherClient;
+        public IndexModel(ILogger<IndexModel> logger, WeatherClient weatherClient)
+        {
+            _logger = logger;
+            _weatherClient = weatherClient;
+        }
+
+        public IList<WeatherInfo> WeatherData { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            
+                WeatherData = (await _weatherClient.GetWeatherInfo()).ToList();
+         
+            return Page();
+        }
+    }
+}
